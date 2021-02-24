00f734cb0bd50ee51531117b8e7f1ae5b6b6927d
LuanSilveiraSouza <luansouzasilveira@gmail.com>
2021-02-24 15:04:45 -0300

    feat: add reverse method to linked list

--- linkedList/linkedList.go
+++ linkedList/linkedList.go
@@ -7,53 +7,61 @@ import (
 
 type Node struct {
 	value interface{}
-	link *Node
+	link  *Node
 }
 
 type LinkedList struct {
 	head *Node
-	tail *Node
 	Size int
 }
 
 func NewLinkedList() LinkedList {
-	return LinkedList{head: nil, tail: nil, Size: 0} 
+	return LinkedList{head: nil, Size: 0}
 }
 
-func (list *LinkedList) Insert(value interface{}) {
+func (list *LinkedList) Insert(value interface{}) bool {
 	node := Node{value: value, link: nil}
 
 	if list.head == nil {
 		list.head = &node
-		list.tail = &node
+		list.Size++
 	} else {
-		list.tail.link = &node
-		list.tail = &node
-	}
+		lastNode, err := list.Search(list.Size - 1)
 
-	list.Size++
+		if err == nil {
+			lastNode.link = &node
+			list.Size++
+			return true
+		}
+	}
+	return false
 }
 
-func (list *LinkedList) Search(position int) (resultNode Node, err error) {
+func (list *LinkedList) Search(position int) (resultNode *Node, err error) {
 	node := list.head
 
-	for i := 0; i < list.Size; i++ {
+	if position >= list.Size || position < 0 {
+		return node, errors.New("Not found")
+	}
+
+	i := 0
+	for {
 		if i == position {
-			return *node, nil
+			return node, nil
 		}
+		i++
 		node = node.link
 	}
-	return *list.tail, errors.New("Not found")
 }
 
-func (list *LinkedList) Delete(position int) (resultNode Node, err error) {
+func (list *LinkedList) Delete(position int) (resultNode *Node, err error) {
 	targetNode, err := list.Search(position)
 
 	if err == nil {
 		node := list.head
-		fmt.Println(*node.link, targetNode)
+
 		for {
-			if *node.link == targetNode {
+			if node.link == targetNode {
 				node.link = targetNode.link
 				list.Size--
 				return targetNode, nil
@@ -61,20 +69,42 @@ func (list *LinkedList) Delete(position int) (resultNode Node, err error) {
 			node = node.link
 		}
 	}
-	return *list.tail, err
+	return list.head, err
+}
+
+func (list *LinkedList) Reverse() {
+	node := list.head
+
+	var previousNode *Node
+	previousNode = nil
+
+	var link *Node
+
+	for {
+		link = node.link
+		node.link = previousNode
+		previousNode = node
+		node = link
+
+		if node.link == nil {
+			node.link = previousNode
+			list.head = node
+			break
+		}
+	}
 }
 
 func (list *LinkedList) Display() {
 	fmt.Printf("head: %p\n", list.head)
 
 	node := list.head
-	
+
 	for {
-		fmt.Printf("value: %v\tnext: %p\n", node.value, node.link)
+		fmt.Printf("value: %v\tlink: %p\n", node.value, node.link)
 
 		if node.link == nil {
 			break
-		}	
+		}
 		node = node.link
 	}
 }
